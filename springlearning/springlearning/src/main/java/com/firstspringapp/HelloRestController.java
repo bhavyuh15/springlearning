eaa9db4c93827fb47a497c16aaf8c4ffd1996227
bhavyuh15 <bhavyadatey15@gmail.com>
2025-02-22 13:50:40 +0530

    [Bhavya] Added springlearning folder in uc2 branch

---
+++ springlearning/springlearning/src/main/java/com/firstspringapp/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.firstspringapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello From Bridgelabz";
+    }
+
+}
